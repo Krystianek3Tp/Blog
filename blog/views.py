c4a991104f5a317624c0d1d76a613f6863c7872b
C04H05 <c04h04@teb.edu.pl>
2024-03-01 13:08:11 +0100

    01.03.24

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from blog.models import Post
+
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request,  "blog/post/list.html")
