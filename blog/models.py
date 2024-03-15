28f9919895479d4931ee2944f1c462c3e1baca67
C04H05 <c04h04@teb.edu.pl>
2024-03-15 13:09:18 +0100

    15.03

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -35,3 +36,8 @@ class Post(models.Model):
 
 
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import  path
 app_name='blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_lists')
+    path('', views.post_list, name='post_lists'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail')
 ]
