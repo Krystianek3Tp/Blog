e96bd8cbbe1bbbb88e301330ae8b326d82cbc2b5
C04H05 <c04h04@teb.edu.pl>
2024-03-22 13:22:21 +0100

    22.03.24 paginacja

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import  path
 app_name='blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_lists'),
+    # path('', views.post_list, name='post_lists'),
+    path('',views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail')
 ]
